73946018e59477aade34ccf244a194f90cd19c11
Jayant Malka <rite2jayantkm@gmail.com>
2025-11-21 20:15:26 +0530

    add changes

--- DSA3.cpp
+++ DSA3.cpp
@@ -6,16 +6,44 @@ int main() {
     int n = 5 ;
     for ( int i = 1 ; i<=n ; i++) 
     {
-        for(int j = 1; j<=i ; j++)
+        for(int j = 1; j<=n ; j++)
         {
-        cout <<  j ; 
+            cout <<  j ; 
         }
-cout << endl ;
+        cout << endl ;
 
     }
+// the pattern where alphabes are printed instead of numbers 
+    for (int i = 1 ; i<=n ; i++)
+    {
+        char ch = 'A' ;
+        for( int l=1 ; l<=n ; l++)
+        {
+            cout << ch ;
+            ch = ch + 1 ;
 
+        }
+        cout << endl ;
+   }
+   
+   int num = 10 ;
 
+   for(int m = 1; m < 4 ; m++)
+   {
+        for(int p=1 ; p<4 ; p++)
+        {
+            cout << num << ' ';
+            num++ ;
+        }
+        cout << endl ;
 
+   }
+
+   
+   cout << "the matrix has been printed" << endl ;
+return 0 ;
+
+    
 
 }
 
