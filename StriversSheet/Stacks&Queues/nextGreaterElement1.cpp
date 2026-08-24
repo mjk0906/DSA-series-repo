b82f5f43943382d50189fe245de8a6292454a0c9
Jayant Malka <rite2jayantkm@gmail.com>
2026-08-24 18:34:44 +0530

    24-08-26

---
+++ StriversSheet/Stacks&Queues/nextGreaterElement1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s ;
+        unordered_map<int,int> mp ;
+
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!s.empty() && s.top()<=nums2[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                mp[nums2[i]]=-1 ;
+            }
+            else{
+                mp[nums2[i]]=s.top();
+            }
+            s.push(nums2[i]);
+        }
+        vector<int> ans ;
+        for(int x : nums1){
+            ans.push_back(mp[x]);
+        }
+        return ans ;
+    }
+};
+
+//easy but intuition is important 
